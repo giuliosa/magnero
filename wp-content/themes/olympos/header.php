25e89a9fa268b889bcfff1fc2cb4dc43dbe94776
giuliosa <giuliocaetano@yahoo.com.br>
2018-01-16 11:04:02 -0300

    adicionando favicon

--- wp-content/themes/olympos/header.php
+++ wp-content/themes/olympos/header.php
@@ -6,11 +6,14 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width">
     <title><?php if(is_home()) bloginfo('name'); else wp_title(''); ?></title>
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon-16x16.png">
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon-32x32.png">
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
     <?php wp_head(); ?>
 
 
-    <meta name="viewport" content="width=device-width">
 </head>
 <body <?php body_class(); ?>>
         
