df9251ec35488ff29d4a0419b2a65ba899e8425b
giuliosa <giuliocaetano@yahoo.com.br>
2018-02-02 16:30:36 -0300

    Adicionando o que precisa para o elementor

---
+++ wp-content/themes/Mjolnir/footer-elementor.php
@@ -0,0 +1,7 @@
+<!-- WP FOOTER -->
+<?php wp_footer(); ?>
+<!-- FIM WP FOOTER -->
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCSy58jX-AnCLhdZw4Msbbhwg6l_8kP0qw&callback=initMap"></script>
+</body>
+
+</html>
