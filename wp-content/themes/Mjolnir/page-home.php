044c229f8eb6a0b92324070add6d839fb787e217
giuliosa <giuliocaetano@yahoo.com.br>
2018-03-14 14:20:38 -0300

    mudando o banner e adicionando o robots.txt

--- wp-content/themes/Mjolnir/page-home.php
+++ wp-content/themes/Mjolnir/page-home.php
@@ -13,13 +13,20 @@ if (have_posts()) : while (have_posts()) : the_post();
 ?>
 
 <section class="banner-marketing">
-    <h1 class='console-container'>
-        Atraia clientes para seu <br>
-        negócio com <br>
-    <span id='text'></span><div class='console-underscore' id='console'>&#95;</div></h1>
+    <div class="banner-marketing__para-frente">
+        <h1 class='console-container'>
+            Atraia clientes para seu <br>
+            negócio com <br>
+        <span id='text'></span><div class='console-underscore' id='console'>&#95;</div></h1>
 
-    <p>Usando as mídias digitais para ajudar seu negócio a crescer</p>
-    <button onClick="location.href='/contato/#se-inscreva'" href="" class="butn">Peça um orçamento</button>
+        <p>Usando as mídias digitais para ajudar seu negócio a crescer</p>
+        <button href="" class="butn">Peça um orçamento</button>
+    </div>
+    
+
+    
+    <svg width="510.5" height="510.5" version="1.1" viewBox="0 0 510.49963 510.49963" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xlink="http://www.w3.org/1999/xlink"><metadata><rdf:RDF><cc:Work rdf:about=""><dc:format>image/svg+xml</dc:format><dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage"/><dc:title/></cc:Work></rdf:RDF></metadata><defs><linearGradient id="b"><stop stop-color="#26053f" offset="0"/><stop stop-color="#f3615f" offset="1"/></linearGradient><linearGradient id="a" x1="66.383" x2="397.23" y1="435.22" y2="69.659" gradientUnits="userSpaceOnUse" xlink:href="#b"/></defs><g transform="matrix(.70711 .70711 -.70711 .70711 270.38 -121.92)" fill="url(#a)"><path d="m424.18 456.17h-69.293c-18.706 0-33.924-15.218-33.924-33.924v-218.94c0-35.941-28.687-65.62-63.949-66.158-17.498-0.302-34.054 6.372-46.566 18.693-12.514 12.327-19.408 28.756-19.408 46.261v67.368c0 7.971-6.463 14.436-14.436 14.436s-14.436-6.464-14.436-14.436v-67.368c0-25.3 9.952-49.036 28.021-66.831 18.064-17.792 41.948-27.389 67.265-26.992 50.94 0.778 92.381 43.407 92.381 95.027v218.94c0 2.786 2.266 5.053 5.053 5.053h69.293c2.786 0 5.053-2.266 5.053-5.053v-58.454c0-7.971 6.463-14.436 14.436-14.436s14.436 6.464 14.436 14.436v58.454c-3e-3 18.706-15.221 33.924-33.926 33.924zm-267.06 0h-69.294c-18.706 0-33.924-15.218-33.924-33.924v-220.14c0-111.44 90.663-202.1 202.1-202.1s202.1 90.663 202.1 202.1v66.883c0 7.971-6.463 14.436-14.436 14.436s-14.436-6.464-14.436-14.436v-66.883c0-95.519-77.711-173.23-173.23-173.23s-173.23 77.71-173.23 173.23v220.14c0 2.786 2.266 5.053 5.053 5.053h69.293c2.786 0 5.053-2.266 5.053-5.053v-71.922c0-7.971 6.463-14.436 14.436-14.436s14.436 6.464 14.436 14.436v71.922c-3e-3 18.706-15.221 33.924-33.925 33.924z"/><path d="m424.18 456.17h-69.293c-18.706 0-33.924-15.218-33.924-33.924v-84.448c0-7.971 6.463-14.436 14.436-14.436h108.27c7.973 0 14.436 6.464 14.436 14.436v84.448c-1e-3 18.706-15.219 33.924-33.924 33.924zm-74.346-103.94v70.012c0 2.786 2.266 5.053 5.053 5.053h69.293c2.786 0 5.053-2.266 5.053-5.053v-70.012h-79.399z"/><path d="m157.12 456.17h-69.294c-18.706 0-33.924-15.218-33.924-33.924v-84.448c0-7.971 6.463-14.436 14.436-14.436h108.27c7.973 0 14.436 6.464 14.436 14.436v84.448c-1e-3 18.706-15.219 33.924-33.923 33.924zm-74.346-103.94v70.012c0 2.786 2.266 5.053 5.053 5.053h69.293c2.786 0 5.053-2.266 5.053-5.053v-70.012h-79.399z"/></g></svg>
+
 </section>
 
 <!-- Seção com os serviços -->
